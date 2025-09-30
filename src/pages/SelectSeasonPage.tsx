1f51fd247acacea798df9d0c92e2326aaeff7d18
Eberechi Omeje <eb.omeje@gmail.com>
2025-09-30 08:20:53 -0600

    modify styling stats and profile page

--- src/pages/SelectSeasonPage.tsx
+++ src/pages/SelectSeasonPage.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import SelectSeason from "../components/Stats/SelectSeason";
 import useSeasonStore from "../stores/seasonStore";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 
 const SelectSeasonPage: React.FC = () => {
   const { fetchSeasons } = useSeasonStore();
@@ -9,12 +12,41 @@ const SelectSeasonPage: React.FC = () => {
     fetchSeasons();
   }, []);
 
+  const cardBgColor =
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "var(--color-ok-200)"
+      : "var(--color-ok-700)";
+
   return (
-    <>
-      <div>
-        <SelectSeason />
-      </div>
-    </>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="80vh"
+    >
+      <Card
+        sx={{
+          minWidth: 300,
+          maxWidth: 500,
+          width: { xs: "50%", sm: "80%", md: "60%", lg: "50%" },
+          p: 2,
+          my: 4,
+          bgcolor: cardBgColor,
+          boxShadow: 3,
+          borderRadius: 2,
+          minHeight: "50vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <CardContent>
+          <SelectSeason />
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
 
